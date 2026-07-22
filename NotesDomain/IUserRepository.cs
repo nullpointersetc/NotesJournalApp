4efad118a79fd0270aac4d70f7e2a29cf1718a69
Darren Morby <nullpointersetc@users.noreply.github.com>
2026-07-22 13:57:59 -0400

    commit0.8: Add UserRepository interface to Domain layer so that the Storage layer knows what to implement.

---
+++ NotesDomain/IUserRepository.cs
@@ -0,0 +1,35 @@
+#region interface IUserRepository
+#pragma warning disable IDE0130
+#pragma warning disable IDE0240
+#nullable enable
+
+using Guid = System.Guid;
+
+using Task_User = System.Threading.Tasks.Task<
+    NullPointersEtc.NotesJournalApp.Entities.User>;
+
+using Task_Optional_User = System.Threading.Tasks.Task<
+    NullPointersEtc.NotesJournalApp.Entities.User?>;
+
+using Task_IEnumerable_User =
+    System.Threading.Tasks.Task<
+    System.Collections.Generic.IEnumerable<
+    NullPointersEtc.NotesJournalApp.Entities.User>>;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace NullPointersEtc.NotesJournalApp.Entities;
+
+interface IUserRepository
+{
+    Task_User CreateAsync(User user);
+    Task_Optional_User GetByIdAsync(Guid userID);
+    Task_Optional_User GetByIdentifierAsync(
+        string identifier);
+    Task_Optional_User GetByDisplayAsync(
+        string display);
+    Task_IEnumerable_User GetAllUsersAsync();
+    Task_User UpdateAsync(User user);
+    Task DeleteAsync(Guid userID);
+}
+#endregion interface IUserRepository
