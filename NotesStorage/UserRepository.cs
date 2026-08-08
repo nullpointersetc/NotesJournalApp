1a524418ebb5f889bdb1c8cf274226097d656fbd
Darren Morby <nullpointersetc@users.noreply.github.com>
2026-08-08 14:10:27 -0400

    commit0.4.4.11: UserRepository now implements IUserRepository with the new names.

--- NotesStorage/UserRepository.cs
+++ NotesStorage/UserRepository.cs
@@ -2,79 +2,93 @@
 #pragma warning disable IDE0001, IDE0130, IDE0240, IDE0290
 #nullable enable
 
-/* To include this "using", you must execute:
-**
-** dotnet add NotesStorage package Microsoft.EntityFrameworkCore
-** dotnet add NotesStorage package Microsoft.EntityFrameworkCore.Sqlite
-** dotnet add NotesStorage package Microsoft.EntityFrameworkCore.SqlServer
-*/
-
-using Microsoft.EntityFrameworkCore;
-
 using User = NullPointersEtc.NotesJournalApp.UserEntity.User;
+using Guid = System.Guid;
+using Task = System.Threading.Tasks.Task;
+
+using TaskReturningUser = System.Threading.Tasks.Task<
+    NullPointersEtc.NotesJournalApp.UserEntity.User>;
+
+using TaskReturningUsers = System.Threading.Tasks.Task<
+    System.Collections.Generic.IEnumerable<
+        NullPointersEtc.NotesJournalApp.UserEntity.User>>;
 
 using IUserRepository =
     NullPointersEtc.NotesJournalApp.UserEntity.IUserRepository;
 
-namespace NullPointersEtc.NotesJournalApp.NotesStorage
+using EntityFrameworkQueryableExtensions =
+    Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions;
+
+namespace NullPointersEtc.NotesJournalApp.NotesStorage;
+
+public sealed class UserRepository : IUserRepository
 {
-    public class UserRepository : IUserRepository
+    public UserRepository(NotesDbContextForSqlite db)
     {
-        public UserRepository(NotesDbContextForSqlite db)
-        {
-            db1 = db;
-        }
-
-        public UserRepository(NotesDbContextForSqlServer db)
-        {
-            db1 = db;
-        }
-
-        async System.Threading.Tasks.Task<User>
-            IUserRepository.CreateAsync(User user)
-        {
-            db1.Users.Add(user);
-            await db1.SaveChangesAsync();
-            return user;
-        }
-
-        System.Threading.Tasks.Task<User>
-            IUserRepository.GetByIdAsync(Guid userID)
-            => db1.Users.FirstAsync(user => user.UserID == userID);
-
-        System.Threading.Tasks.Task<User>
-            IUserRepository.GetByIdentifierAsync(
-                string identifier)
-            => db1.Users.FirstAsync(user => user.UserName==identifier);
-
-        System.Threading.Tasks.Task<User>
-            IUserRepository.GetByDisplayAsync(string display)
-            => db1.Users.FirstAsync(user => user.Display == display);
-
-        async System.Threading.Tasks.Task<
-            System.Collections.Generic.IEnumerable<User>>
-            IUserRepository.GetAllUsersAsync()
-            => await db1.Users.ToListAsync();
-
-        async System.Threading.Tasks.Task<User>
-            IUserRepository.UpdateAsync(User user)
-        {
-            db1.Users.Update(user);
-            await db1.SaveChangesAsync();
-            return user;
-        }
-
-        async Task IUserRepository.DeleteAsync(
-            Guid id)
-        {
-            User user = await db1.Users.FirstAsync(user => user.UserID == id);
-            db1.Users.Remove(user);
-            await db1.SaveChangesAsync();
-        }
-
-        private readonly NotesDbContext db1;
-
+        myDB = db;
     }
+
+
+    public UserRepository(NotesDbContextForSqlServer db)
+    {
+        myDB = db;
+    }
+
+
+    public async TaskReturningUser
+        CreateUserAsync(User user)
+    {
+        myDB.Users.Add(user);
+        await myDB.SaveChangesAsync();
+        return user;
+    }
+
+
+    public TaskReturningUser GetUserByUserIdAsync(Guid userID)
+        => EntityFrameworkQueryableExtensions.FirstAsync(
+            myDB.Users, predicate: user => user.UserID == userID);
+
+
+    public TaskReturningUser GetUserByUserNameAsync(
+            string userName)
+        => EntityFrameworkQueryableExtensions.FirstAsync(
+            myDB.Users,
+            predicate: user => user.UserName == userName);
+
+
+    public TaskReturningUser GetUserByDisplayNameAsync(
+        string displayName)
+        => EntityFrameworkQueryableExtensions.FirstAsync(
+            myDB.Users,
+            predicate: user => user.DisplayName == displayName);
+
+
+    public async TaskReturningUsers GetAllUsersAsync()
+        => await EntityFrameworkQueryableExtensions.ToListAsync<User>(
+            myDB.Users);
+
+
+    public async TaskReturningUser UpdateUserAsync(User user)
+    {
+        myDB.Users.Update(user);
+        await myDB.SaveChangesAsync();
+        return user;
+    }
+
+
+    public async Task DeleteUserAsync(
+        Guid userID)
+    {
+        User user = await EntityFrameworkQueryableExtensions.FirstAsync(
+            myDB.Users,
+            predicate: user => user.UserID == userID);
+
+        myDB.Users.Remove(user);
+        await myDB.SaveChangesAsync();
+    }
+
+
+    private readonly NotesDbContext myDB;
 }
 
 #endregion "UserRepository.cs"
