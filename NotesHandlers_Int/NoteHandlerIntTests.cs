b681e96f4932d401256d8cc166d467206387b6cf
Darren Morby <nullpointersetc@users.noreply.github.com>
2026-08-10 16:11:01 -0400

    commit0.4.4.21: Add IntegrationTests for SQLite.

---
+++ NotesHandlers_Int/NoteHandlerIntTests.cs
@@ -0,0 +1,111 @@
+﻿﻿#region "NotesHandlers_Int/NoteHandlerIntTests.cs"
+#pragma warning disable IDE0130, xUnit2012
+
+using Assert = Xunit.Assert;
+using Fact = Xunit.FactAttribute;
+using Guid = System.Guid;
+using InvalidOperationException = System.InvalidOperationException;
+using NotesDbContextForSqlite = NullPointersEtc.NotesJournalApp.NotesStorage.NotesDbContextForSqlite;
+using NoteRepository = NullPointersEtc.NotesJournalApp.NotesStorage.NoteRepository;
+using NoteHandler = NullPointersEtc.NotesJournalApp.NotesHandlers.NoteHandler;
+using Note = NullPointersEtc.NotesJournalApp.NoteEntity.Note;
+using Task = System.Threading.Tasks.Task;
+
+using SqliteDbContextOptionsBuilderExtensions =
+    Microsoft.EntityFrameworkCore.SqliteDbContextOptionsBuilderExtensions;
+
+using DbContextOptionsBuilderNotesDbContextForSqlite =
+    Microsoft.EntityFrameworkCore.DbContextOptionsBuilder<
+        NullPointersEtc.NotesJournalApp.NotesStorage.NotesDbContextForSqlite>;
+
+using RelationalDatabaseFacadeExtensions =
+    Microsoft.EntityFrameworkCore.RelationalDatabaseFacadeExtensions;
+
+using Notes = System.Collections.Generic.IEnumerable<
+    NullPointersEtc.NotesJournalApp.NoteEntity.Note>;
+
+namespace NullPointersEtc.NotesJournalApp.NotesHandlers_IntegrationTests;
+
+public class NoteHandlerIntegrationTests
+{
+    private static NotesDbContextForSqlite CreateDb()
+    {
+        DbContextOptionsBuilderNotesDbContextForSqlite options =
+            SqliteDbContextOptionsBuilderExtensions.UseSqlite(
+            new DbContextOptionsBuilderNotesDbContextForSqlite(),
+            connectionString: "Data Source=:memory:");
+
+        NotesDbContextForSqlite db = new(options.Options);
+        RelationalDatabaseFacadeExtensions.OpenConnection(db.Database);
+        db.Database.EnsureCreated();
+        return db;
+    }
+
+    [method: @Fact]
+    public async Task CreateNotePersistsToSQLiteAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        NoteRepository repo = new(db);
+        NoteHandler handler = new(repo);
+
+        Note note = await handler.CreateNoteWithHandlerAsync("Title1", "Body1");
+
+        Assert.NotEqual(Guid.Empty, note.NoteID);
+        Assert.Equal("Title1", note.Title);
+        Assert.Equal("Body1", note.Body);
+
+        var fromDb = await repo.GetNoteByIdAsync(note.NoteID);
+        Assert.Equal("Title1", fromDb.Title);
+    }
+
+    [method: @Fact]
+    public async Task UpdateNoteChangesPersistInSQLiteAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        NoteRepository repo = new(db);
+        NoteHandler handler = new(repo);
+
+        Note note = await handler.CreateNoteWithHandlerAsync("Old", "OldBody");
+
+        Note updated = await handler.UpdateNoteWithHandlerAsync(
+            note.NoteID, "New", "NewBody");
+
+        Assert.Equal("New", updated.Title);
+        Assert.Equal("NewBody", updated.Body);
+
+        Note fromDb = await repo.GetNoteByIdAsync(note.NoteID);
+        Assert.Equal("New", fromDb.Title);
+    }
+
+    [method: @Fact]
+    public async Task SearchNotesFindsMatchesAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        NoteRepository repo = new(db);
+        NoteHandler handler = new(repo);
+
+        await handler.CreateNoteWithHandlerAsync("Alpha", "Body");
+        await handler.CreateNoteWithHandlerAsync("Beta", "Body");
+        await handler.CreateNoteWithHandlerAsync("Gamma", "Body");
+
+        Notes results = await handler.SearchNotesWithHandlerAsync("a");
+        Assert.True(System.Linq.Enumerable.Any(results, note => note.Title == "Alpha"));
+        Assert.True(System.Linq.Enumerable.Any(results, note => note.Title == "Gamma"));
+    }
+
+    [method: @Fact]
+    public async Task DeleteNoteRemovesFromSQLiteAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        NoteRepository repo = new(db);
+        NoteHandler handler = new(repo);
+
+        Note note = await handler.CreateNoteWithHandlerAsync("T", "B");
+
+        await handler.DeleteNoteWithHandlerAsync(note.NoteID);
+
+        await Assert.ThrowsAsync<InvalidOperationException>(
+            () => repo.GetNoteByIdAsync(note.NoteID));
+    }
+}
+#endregion "NotesHandlers_Int/NoteHandlerIntTests.cs"
---
+++ NotesHandlers_Int/UserHandlerIntTests.cs
@@ -0,0 +1,108 @@
+﻿﻿#region "NotesHandlers_Int/UserHandlerIntTests.cs"
+#pragma warning disable IDE0130
+
+using Assert = Xunit.Assert;
+using DbUpdateException = Microsoft.EntityFrameworkCore.DbUpdateException;
+using Fact = Xunit.FactAttribute;
+using RelationalDatabaseFacadeExtensions = Microsoft.EntityFrameworkCore.RelationalDatabaseFacadeExtensions;
+using NotesDbContextForSqlite = NullPointersEtc.NotesJournalApp.NotesStorage.NotesDbContextForSqlite;
+using UserRepository = NullPointersEtc.NotesJournalApp.NotesStorage.UserRepository;
+using UserHandler = NullPointersEtc.NotesJournalApp.NotesHandlers.UserHandler;
+using User = NullPointersEtc.NotesJournalApp.UserEntity.User;
+using System;
+using Task = System.Threading.Tasks.Task;
+
+using DbContextOptionsBuilderNotesDbContextForSqlite =
+    Microsoft.EntityFrameworkCore.DbContextOptionsBuilder<
+        NullPointersEtc.NotesJournalApp.NotesStorage.NotesDbContextForSqlite>;
+
+using SqliteDbContextOptionsBuilderExtensions =
+    Microsoft.EntityFrameworkCore.SqliteDbContextOptionsBuilderExtensions;
+
+namespace NullPointersEtc.NotesJournalApp.NotesHandlers_IntegrationTests;
+
+public class UserHandlerIntegrationTests
+{
+    private static NotesDbContextForSqlite CreateDb()
+    {
+        DbContextOptionsBuilderNotesDbContextForSqlite options =
+            SqliteDbContextOptionsBuilderExtensions.UseSqlite(
+                new DbContextOptionsBuilderNotesDbContextForSqlite(),
+                    connectionString: "Data Source=:memory:");
+
+        NotesDbContextForSqlite db = new(options.Options);
+        RelationalDatabaseFacadeExtensions.OpenConnection(db.Database);
+        db.Database.EnsureCreated();
+        return db;
+    }
+
+
+    [method: @Fact]
+    public async Task CreateUserPersistsToSQLiteAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        UserRepository repo = new(db);
+        UserHandler handler = new(repo);
+
+        User user = await handler.CreateUserWithHandlerAsync(
+            "user1", "User One", "user1@mail.com");
+
+        Assert.Equal("user1", user.UserName);
+
+        User actual = await repo.GetUserByUserIdAsync(user.UserID);
+        Assert.Equal("User One", actual.DisplayName);
+    }
+
+
+    [method: @Fact]
+    public async Task UpdateUserPersistsChangesAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        UserRepository repo = new(db);
+        UserHandler handler = new(repo);
+
+        User user = await handler.CreateUserWithHandlerAsync(
+            "user1", "OldName", "old@mail.com");
+
+        User updated = await handler.UpdateUserWithHandlerAsync(
+            user.UserID, "NewName", "new@mail.com");
+
+        Assert.Equal("NewName", updated.DisplayName);
+
+        User actual = await repo.GetUserByUserIdAsync(user.UserID);
+        Assert.Equal("NewName", actual.DisplayName);
+    }
+
+
+    [method: @Fact]
+    public async Task UniqueUserNameConstraintIsEnforcedAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        UserRepository repo = new(db);
+        UserHandler handler = new(repo);
+
+        await handler.CreateUserWithHandlerAsync(
+            "user1", "User One", "one@mail.com");
+
+        await Assert.ThrowsAsync<DbUpdateException>(
+            () => handler.CreateUserWithHandlerAsync(
+                "user1", "Duplicate", "dup@mail.com"));
+    }
+
+    [method: @Fact]
+    public async Task DeleteUserRemovesFromSQLiteAsync()
+    {
+        NotesDbContextForSqlite db = CreateDb();
+        UserRepository repo = new(db);
+        UserHandler handler = new(repo);
+
+        var user = await handler.CreateUserWithHandlerAsync(
+            "user1", "User One", "one@mail.com");
+
+        await handler.DeleteWithHandlerAsync(user.UserID);
+
+        await Assert.ThrowsAsync<InvalidOperationException>(
+            () => repo.GetUserByUserIdAsync(user.UserID));
+    }
+}
+#endregion "NotesHandlers_Int/UserHandlerIntTests.cs"
